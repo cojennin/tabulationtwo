1b3694defcf80e7d78ae39649547814a14d45345
Connor Jennings <cojennin@gmail.com>
2012-12-03 19:23:09 -0500

    Initial commit for second go at Tabulation project. Interesting thing to note: When attempting to text just a basic xul file by dragging and dropping it into firefox, you'll find that the gBrowser global variable does not exist. Currently, you actually have to load the xpi application into firefox in order to utilize it. This seems annoying. Will attempt to figure out an easier way to debug.

---
+++ src/content/sidebar.js
@@ -0,0 +1,21 @@
+
+//$(document).ready(function(){
+	var tab_listener = new TabulationTabListener();
+	//var tab_browser = gBrowser;
+	
+	//console.log(tab_browser);
+
+		function TabulationTabListener(){
+			var sidebar_h3 = $("#tabulation-doc-name");
+			alert(gBrowser.browsers.length);
+			console.log(tab_browser);
+			if(document.title){
+				sidebar_h3.text(document.title);
+				console.log(sidebar_h3.html());
+			}
+			else{
+				sidebar_h3.text("Test test test \ntest test test \n test test");
+				console.log(sidebar_h3.html());
+			}
+		}
+//});
