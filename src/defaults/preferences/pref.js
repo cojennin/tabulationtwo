2fc47253c41e7ec1c55b87a1dc8dfa94fd013ccb
Connor Jennings <cojennin@gmail.com>
2012-12-09 02:44:54 -0500

    Moved pref.js to correct folder.

---
+++ src/defaults/preferences/pref.js
@@ -0,0 +1,7 @@
+pref("extensions.tabulation.show_open_all_time", 1);
+pref("extensions.tabulation.show_closed_all_time", 1);
+pref("extensions.tabulation.show_switched_all_time", 1);
+
+pref("extensions.tabulation.limit_open_tabs", 10);
+pref("extensions.tabulation.clear_tab_data", 0);
+
